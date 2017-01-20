341d03acac5dab8adfdaf5b0d16d21297e43a867
Ravi Punj <ravipunj@Ravis-MacBook-Air-2.local>
2017-01-20 02:17:36 -0800

    Added QPX Search Request Definitions.

--- definitions/qpx.py
+++ definitions/qpx.py
@@ -1,3 +1,8 @@
 class QPXServiceDefinitions(object):
     QPXServiceName = "qpxExpress"
     QPXServiceVersionName = "v1"
+
+class QPXSeachRequestDefinitions(object):
+    PassengerCounts = "qpxexpress#passengerCounts"
+    SliceInput = "qpxexpress#sliceInput"
+    TimeOfDayRange = "qpxexpress#timeOfDayRange"
