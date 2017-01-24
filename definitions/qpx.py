2adec3ee7c88a8ad90d7fb5c63f47ee487f17cca
Ravi Punj <ravipunj@Ravis-MacBook-Air-2.local>
2017-01-24 02:36:41 -0800

    Moved qpx definitions to helpers module.

--- definitions/qpx.py
+++
@@ -1,8 +0,0 @@
-class QPXServiceDefinitions(object):
-    QPXServiceName = "qpxExpress"
-    QPXServiceVersionName = "v1"
-
-class QPXSeachRequestDefinitions(object):
-    PassengerCounts = "qpxexpress#passengerCounts"
-    SliceInput = "qpxexpress#sliceInput"
-    TimeOfDayRange = "qpxexpress#timeOfDayRange"
---
+++ helpers/qpx/definitions.py
@@ -0,0 +1,20 @@
+"""
+Definitions for use in queries to QPX Express by Google.
+"""
+
+
+class QPXServiceDefinitions(object):
+    """
+    Definitions for accessing QPX Express by Google.
+    """
+    QPXServiceName = "qpxExpress"
+    QPXServiceVersionName = "v1"
+
+
+class QPXSeachRequestDefinitions(object):
+    """
+    Definitions for use in creating search requests in APIs for QPX Express by Google.
+    """
+    PassengerCounts = "qpxexpress#passengerCounts"
+    SliceInput = "qpxexpress#sliceInput"
+    TimeOfDayRange = "qpxexpress#timeOfDayRange"
